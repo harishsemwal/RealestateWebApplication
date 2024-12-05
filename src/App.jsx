5fffdc66f81db5826a302e88cbfe49b7161f098f
Harishsemwal <harishsemwal581@gmail.com>
2024-12-06 00:00:53 +0530

    Header Part of Page
    Header Part of Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='w-full overflow-hidden'>
+      <Header/>
     </div>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden' id="About">
+        <h1>About <span></span></h1> 
+    </div>
+  )
+}
+ className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden' id="About"
+export default Ab
+<h1>About <span> </span></h1>
